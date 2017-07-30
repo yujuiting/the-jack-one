c9d41a1fb5286fb78f06f6fdcdff5a07da84d0fe
yujuiting <jt@17.media>
2017-07-31 01:57:53 +0800

    mock browser env for testing

--- scripts/test-environment.js
+++ scripts/test-environment.js
@@ -1,1 +1,17 @@
-require('reflect-metadata');
+const Mocha = require('mocha');
+
+const _run = Mocha.prototype.run;
+
+Mocha.prototype.run = function (...args) {
+
+  require('reflect-metadata');
+
+  /**
+   * Provide mock browser
+   */
+  const { JSDOM } = require('jsdom');
+  const { window } = new JSDOM();
+  global.window = window;
+
+  _run.apply(this, args);
+};
